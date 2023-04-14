1f9b23dcc85d079222e04d7359757f3a944b261f
sltruman <sl.truman@live.com>
2023-04-14 17:58:59 +0800

    模板界面

--- src/app.cpp
+++ src/app.cpp
@@ -22,13 +22,26 @@ using namespace digitaltwin;
 
 struct TemplateView : public Gtk::ScrolledWindow
 {
+    sigc::signal<void()> signal_selected;
+
     Glib::RefPtr<Gtk::FlowBox> template_list;
+    Glib::RefPtr<Gtk::Button> template_1;
+    Glib::RefPtr<Gtk::Button> template_2;
+    Glib::RefPtr<Gtk::Button> template_3;
+    Glib::RefPtr<Gtk::Button> template_4;
 
     TemplateView(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& builder)
         : Gtk::ScrolledWindow(cobject)
         , template_list(builder->get_widget<Gtk::FlowBox>("template_list"))
+        , template_1(builder->get_widget<Gtk::Button>("template_1"))
+        , template_2(builder->get_widget<Gtk::Button>("template_2"))
+        , template_3(builder->get_widget<Gtk::Button>("template_3"))
+        , template_4(builder->get_widget<Gtk::Button>("template_4"))
     {
-        
+        template_1->signal_clicked().connect(sigc::mem_fun(signal_selected,&sigc::signal<void()>::emit));
+        template_2->signal_clicked().connect(sigc::mem_fun(signal_selected,&sigc::signal<void()>::emit));
+        template_3->signal_clicked().connect(sigc::mem_fun(signal_selected,&sigc::signal<void()>::emit));
+        template_4->signal_clicked().connect(sigc::mem_fun(signal_selected,&sigc::signal<void()>::emit));
     }
 };
 
@@ -49,74 +62,78 @@ struct SceneView : public Gtk::Overlay
         delete scene;
     }
 
-    // void open() {
-    //     auto dialog = make_shared<Gtk::FileChooserDialog>("Please choose a file",Gtk::FileChooser::Action::OPEN);
-    //     dialog->add_button("_Open", Gtk::ResponseType::OK);
-    //     dialog->set_transient_for(*this);
-    //     dialog->set_modal();
-    //     dialog->set_visible();
-    //     auto filter_text = Gtk::FileFilter::create();
-    //     filter_text->set_name("*.json");
-    //     filter_text->add_pattern("*.json");
-    //     dialog->add_filter(filter_text);
-    //     dialog->signal_response().connect([this,dialog](int response_id) {
-    //         dialog->close();
+    void open() {
+        cout << "11111111111111111" << endl;
+        // auto dialog = make_shared<Gtk::FileChooserDialog>("Please choose a file",Gtk::FileChooser::Action::OPEN);
+        // dialog->add_button("_Open", Gtk::ResponseType::OK);
+        // // dialog->set_transient_for();
+        // dialog->set_modal();
+        // dialog->set_visible();
+        // auto filter_text = Gtk::FileFilter::create();
+        // filter_text->set_name("*.json");
+        // filter_text->add_pattern("*.json");
+        // dialog->add_filter(filter_text);
 
-    //         if(response_id != Gtk::ResponseType::OK) return;
-    //         auto scene_path = dialog->get_file()->get_path();
-    //         cout << response_id << ' ' << dialog->get_file()->get_path() << endl;
+        // dialog->signal_response().connect([this,dialog](int response_id) {
+        //     dialog->close();
+
+        //     if(response_id != Gtk::ResponseType::OK) return;
+        //     auto scene_path = dialog->get_file()->get_path();
+        //     cout << response_id << ' ' << dialog->get_file()->get_path() << endl;
             
-    //         scene = new Scene(800,640,scene_path);
-    //         editor = new Editor(scene);
-    //         workflow = new Workflow(scene);
-    //         scene_view->area->set_draw_func(sigc::mem_fun(*scene_view, &SceneView::area_paint_event));
-    //         // Robot* robot = dynamic_cast<Robot*>(scene->get_active_objs()["robot"]);
+        //     scene = new Scene(800,640,scene_path);
+        //     editor = new Editor(scene);
+        //     workflow = new Workflow(scene);
+        //     scene_view->area->set_draw_func(sigc::mem_fun(*scene_view, &SceneView::area_paint_event));
+        //     // Robot* robot = dynamic_cast<Robot*>(scene->get_active_objs()["robot"]);
 
-    //         // Camera3D* camera = dynamic_cast<Camera3D*>(scene->get_active_objs()["camera"]);
-    //         // camera->set_rtt_func([](vector<unsigned char> rgba_pixels,vector<float> depth_pixels,int width,int height) {
-    //         //     cv::Mat rgba = cv::Mat::zeros(cv::Size(width,height),CV_8UC4);
-    //         //     memcpy(rgba.ptr<unsigned char>(),rgba_pixels.data(),rgba_pixels.size());
-    //         //     cv::cvtColor(rgba,rgba,cv::COLOR_RGBA2BGRA);
+        //     // Camera3D* camera = dynamic_cast<Camera3D*>(scene->get_active_objs()["camera"]);
+        //     // camera->set_rtt_func([](vector<unsigned char> rgba_pixels,vector<float> depth_pixels,int width,int height) {
+        //     //     cv::Mat rgba = cv::Mat::zeros(cv::Size(width,height),CV_8UC4);
+        //     //     memcpy(rgba.ptr<unsigned char>(),rgba_pixels.data(),rgba_pixels.size());
+        //     //     cv::cvtColor(rgba,rgba,cv::COLOR_RGBA2BGRA);
 
-    //         //     cv::Mat depth = cv::Mat::zeros(cv::Size(width,height),CV_32FC1);
-    //         //     memcpy(depth.ptr<unsigned char>(),depth_pixels.data(),depth_pixels.size() * 4);
-    //         //     cv::Mat gray = cv::Mat::zeros(cv::Size(width,height),CV_8UC1);
-    //         //     depth.convertTo(gray, CV_8U, 255.0);
-    //         //     cv::imwrite("rgba.png",rgba);
-    //         //     cv::imwrite("depth.png",gray);
-    //         // });
+        //     //     cv::Mat depth = cv::Mat::zeros(cv::Size(width,height),CV_32FC1);
+        //     //     memcpy(depth.ptr<unsigned char>(),depth_pixels.data(),depth_pixels.size() * 4);
+        //     //     cv::Mat gray = cv::Mat::zeros(cv::Size(width,height),CV_8UC1);
+        //     //     depth.convertTo(gray, CV_8U, 255.0);
+        //     //     cv::imwrite("rgba.png",rgba);
+        //     //     cv::imwrite("depth.png",gray);
+        //     // });
             
-    //         // Camera3DReal* camera = dynamic_cast<Camera3DReal*>(scene->get_active_objs()["camera"]);
-    //         // camera->set_calibration("[[3507.176621132752,0.0,1218.55397352377],[0.0,3506.5191735910958,1038.1997907533078],[0.0,0.0,1.0]]",
-    //         //                         "[[0.03775400213484963,0.9992836506766881,-0.002611668637494993,0.18975821360828188],[0.9946931066717141,-0.03783062740787925,-0.09567897976933247,-0.8562761592934359],[-0.09570924126005752,0.0010144556158444772,-0.995408816525767,1.1566388127904739],[0,0,0,1]]");
+        //     // Camera3DReal* camera = dynamic_cast<Camera3DReal*>(scene->get_active_objs()["camera"]);
+        //     // camera->set_calibration("[[3507.176621132752,0.0,1218.55397352377],[0.0,3506.5191735910958,1038.1997907533078],[0.0,0.0,1.0]]",
+        //     //                         "[[0.03775400213484963,0.9992836506766881,-0.002611668637494993,0.18975821360828188],[0.9946931066717141,-0.03783062740787925,-0.09567897976933247,-0.8562761592934359],[-0.09570924126005752,0.0010144556158444772,-0.995408816525767,1.1566388127904739],[0,0,0,1]]");
 
-    //         // camera->set_rtt_func([](vector<unsigned char>& rgb_pixels,vector<float>& depth_pixels,int& width,int& height) {
-    //         //     width = 1920,height = 1200;
+        //     // camera->set_rtt_func([](vector<unsigned char>& rgb_pixels,vector<float>& depth_pixels,int& width,int& height) {
+        //     //     width = 1920,height = 1200;
 
-    //         //     std::string sRGBFilePath = "/home/truman/Downloads/20230322110752009.png";
-    //         //     cv::Mat rgbMat = cv::imread(sRGBFilePath);
-    //         //     rgb_pixels = rgbMat.reshape(1,1);
+        //     //     std::string sRGBFilePath = "/home/truman/Downloads/20230322110752009.png";
+        //     //     cv::Mat rgbMat = cv::imread(sRGBFilePath);
+        //     //     rgb_pixels = rgbMat.reshape(1,1);
                 
-    //         //     std::string sDepthFilePath = "/home/truman/Downloads/20230322110752009.tiff";
-    //         //     cv::Mat depthMat = cv::imread(sDepthFilePath, cv::IMREAD_ANYDEPTH);
-    //         //     depth_pixels = depthMat.reshape(1,1);
+        //     //     std::string sDepthFilePath = "/home/truman/Downloads/20230322110752009.tiff";
+        //     //     cv::Mat depthMat = cv::imread(sDepthFilePath, cv::IMREAD_ANYDEPTH);
+        //     //     depth_pixels = depthMat.reshape(1,1);
 
-    //         //     width = rgbMat.cols;
-    //         //     height = rgbMat.rows;
-    //         //     return true;
-    //         // });
+        //     //     width = rgbMat.cols;
+        //     //     height = rgbMat.rows;
+        //     //     return true;
+        //     // });
 
-    //         // workflow->add_active_obj_node("Vision","PickLight","detect",[](){
-    //         //     return "[[[-0.6109328215852812,-0.770627694607955,-0.18136690351319718,-0.16305102293176799],[-0.7829483866689572,0.6220597556471429,-0.0057755018884309605,-0.03409599941935853],[0.11727181739493445,0.13847249629497058,-0.9833984376293907,1.2207300122066735],[0.0,0.0,0.0,1.0]]]";
-    //         // });
+        //     // workflow->add_active_obj_node("Vision","PickLight","detect",[](){
+        //     //     return "[[[-0.6109328215852812,-0.770627694607955,-0.18136690351319718,-0.16305102293176799],[-0.7829483866689572,0.6220597556471429,-0.0057755018884309605,-0.03409599941935853],[0.11727181739493445,0.13847249629497058,-0.9833984376293907,1.2207300122066735],[0.0,0.0,0.0,1.0]]]";
+        //     // });
         
-    //         // camera->rtt();
+        //     // camera->rtt();
 
-    //         auto controller = Gtk::EventControllerLegacy::create();
-    //         controller->signal_event().connect(sigc::mem_fun(*scene_view,&SceneView::area_drag_event),false);
-    //         scene_view->add_controller(controller);
-    //     });
-    // }
+        //     auto controller = Gtk::EventControllerLegacy::create();
+        //     controller->signal_event().connect(sigc::mem_fun(*scene_view,&SceneView::area_drag_event),false);
+        //     scene_view->add_controller(controller);
+        // });
+
+    }
+
 
     // int handled = 0;
     // Glib::RefPtr<const Gdk::Event> prev_ev;
@@ -243,10 +260,13 @@ public:
         button_stop->signal_clicked().connect(sigc::mem_fun(*this,&AppWindow::on_button_stop_clicked));
         auto button_workflow = builder->get_widget<Gtk::Button>("workflow");
         button_workflow->signal_clicked().connect(sigc::mem_fun(*this,&AppWindow::on_button_workflow_clicked));
+
+        template_view->signal_selected.connect(sigc::mem_fun(*scene_view,&SceneView::open));
     }
 
     ~AppWindow()
     {
+
     }
 
     void on_button_open_clicked()
